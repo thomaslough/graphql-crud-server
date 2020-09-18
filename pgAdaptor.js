5e662e24704ac87c3595bbc3b274bf0d1c5b64cd
Thomas Lough <thomas@tmldesign.com>
2020-09-17 18:20:26 -0700

    Adds dynamic db name from env

--- pgAdaptor.js
+++ pgAdaptor.js
@@ -5,7 +5,7 @@ const pgp = pgPromise({});
 const config = {
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
+  database: process.env.DB_NAME || rocess.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
 };
